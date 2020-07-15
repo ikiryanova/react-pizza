a75989d61b868f09750dfd19ed93a7f2c7070fee
Irina <irinakir98@gmail.com>
2020-07-15 18:23:04 +0300

    Header, Button, Categories

---
+++ src/components/Categories.jsx
@@ -0,0 +1,62 @@
+import React from 'react';
+
+// class Categories extends React.Component {
+  
+//   state = {
+//     activeItem: 1,
+//   };
+
+//   onSelectItem = index => {
+//     this.setState({
+//       activeItem: index,    // обновляет состояние
+//     }); 
+//   }
+
+//   render() {
+//     const { items } = this.props;
+//     return (
+//       <div>
+//         <div className="categories">
+//           <ul>
+//             <li className="active">Все</li>
+//             {
+//               items.map((item, index) => (
+//                 <li className={this.state.activeItem === index ? 'active' : ''} onClick={() => this.onSelectItem(index)} key={`${item}__${index}`}>{item}</li>
+//               ))
+//             }
+//           </ul>
+//         </div>
+//       </div>
+//     )
+//   };
+// }
+
+function Categories({ items }) {
+  const [activeItem, setActiveItem] = React.useState(0);
+
+  const onSelectItem = (index) => {
+    setActiveItem(index);
+  }
+
+  return (
+    <div>
+      <div className="categories">
+        <ul>
+          {items &&
+            items.map((item, index) => (
+              <li 
+                className={activeItem === index ? 'active' : ''}
+                onClick={() => onSelectItem(index)} 
+                key={`${item}__${index}`}
+              >
+                {item}
+              </li>
+            ))
+          }
+        </ul>
+      </div> 
+    </div>
+  );
+}
+
+export default Categories;
